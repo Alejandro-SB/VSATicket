aa07067e627b239604ceb1f311c808083ea9e69d
havranekj <havryjr+neco@gmail.com>
2024-12-04 00:59:07 +0100

    adding Delete ticket

---
+++ VSATicket/Application/Features/Tickets/DeleteTicket/DeleteTicketCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace VSATicket.Application.Features.Tickets.DeleteTicket
+{
+    public class DeleteTicketCommand
+    {
+        public int Id { get; set; }
+
+        public DeleteTicketCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
