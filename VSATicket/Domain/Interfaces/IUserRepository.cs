33f67ff1f5c9f8676ff4b84549e14a1bd5dd0850
havranekj <havryjr+neco@gmail.com>
2024-12-05 10:44:18 +0100

    adding models, repository and interface for users

---
+++ VSATicket/Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using VSATicket.Domain.Common.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace VSATicket.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IdentityResult> CreateUserAsync(AppUser user, string password);
+        Task<AppUser?> GetUserByNameAsync(string userName);
+        Task<SignInResult> PasswordSignInAsync(string userName, string password);
+        Task AddToRoleAsync(AppUser user, string role);
+    }
+}
---
+++ VSATicket/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using VSATicket.Domain.Common.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using VSATicket.Application.Interfaces;
+
+namespace VSATicket.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public UserRepository(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IdentityResult> CreateUserAsync(AppUser user, string password)
+        {
+            return await _userManager.CreateAsync(user, password);
+        }
+
+        public async Task<AppUser?> GetUserByNameAsync(string userName)
+        {
+            return await _userManager.Users.SingleOrDefaultAsync(u => u.UserName == userName);
+        }
+
+        public async Task<SignInResult> PasswordSignInAsync(string userName, string password)
+        {
+            return await _signInManager.PasswordSignInAsync(userName, password, false, false);
+        }
+
+        public async Task AddToRoleAsync(AppUser user, string role)
+        {
+            await _userManager.AddToRoleAsync(user, role);
+        }
+    }
+}
