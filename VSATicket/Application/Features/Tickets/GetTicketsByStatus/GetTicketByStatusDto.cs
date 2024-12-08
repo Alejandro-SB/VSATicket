5649f3de89076fc76185bf2beb4f1c9e576a2889
havranekj <havryjr+neco@gmail.com>
2024-12-09 00:54:35 +0100

    adding feature to get ticket by status

---
+++ VSATicket/Application/Features/Tickets/GetTicketsByStatus/GetTicketByStatusDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VSATicket.Application.Features.Tickets.GetTicketsByStatus
+{
+    public class GetTicketByStatusDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Status { get; set; }
+    }
+}
